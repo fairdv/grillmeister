7c1b71d160e5038bc4ad2410fb58ba377a29902f
fairdv <info@fairdv.de>
2016-07-04 16:09:54 +0200

    Screen Main

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition
+from kivy.properties import ObjectProperty
 
 
-grillmeister = App()
-print 'Meine Anwendung'
-grillmeister.run()
+
+class ScreenEins(Screen):
+	pass
+
+class ScreenZwei(Screen):
+	pass
+
+class ScreenDrei(Screen):
+	pass
+	
+class ScreenVier(Screen):
+	pass
+
+class Manager(ScreenManager):
+	
+	screen_eins = ObjectProperty(None)
+	screen_zwei = ObjectProperty(None)
+	screen_drei = ObjectProperty(None)
+	screen_vier = ObjectProperty(None)
+	
+
+class ScreensApp(App):
+	
+	def build(self):
+		m = Manager(transition= WipeTransition())
+		return m
+
+if __name__ == "__main__":
+	ScreensApp().run()
+
+	
